d7778a1c46a4ab5878a614a6d67255168adf8212
Tom <tjpalmer@tjpalmer.com>
2017-11-01 05:30:39 -0700

    Test nesting

--- src/main.rs
+++ src/main.rs
@@ -27,17 +27,20 @@ fn parse() -> Result<(), io::Error> {
   // Because ident_list is silent, the iterator will contain idents
   for pair in pairs {
     // A pair is a combination of the rule which matched and a span of input
-    println!("Rule:    {:?}", pair.as_rule());
-    println!("Span:    {:?}", pair.clone().into_span());
-    println!("Text:    {}", pair.clone().into_span().as_str());
+    println!("Rule: {:?}", pair.as_rule());
+    println!("Span: {:?}", pair.clone().into_span());
+    println!("Text: {}", pair.clone().into_span().as_str());
 
     // A pair can be converted to an iterator of the tokens which make it up:
     for inner_pair in pair.into_inner() {
-      match inner_pair.as_rule() {
-        Rule::alpha => println!("Letter:  {}", inner_pair.into_span().as_str()),
-        Rule::digit => println!("Digit:   {}", inner_pair.into_span().as_str()), 
-        _ => unreachable!()
-      };
+      println!("  Rule: {:?}", inner_pair.as_rule());
+      println!("  Span: {:?}", inner_pair.clone().into_span());
+      println!("  Text: {}", inner_pair.clone().into_span().as_str());
+    //   match inner_pair.as_rule() {
+    //     Rule::alpha => println!("Letter:  {}", inner_pair.into_span().as_str()),
+    //     Rule::digit => println!("Digit:   {}", inner_pair.into_span().as_str()), 
+    //     _ => unreachable!()
+    //   };
     }
   };
   Ok(())
