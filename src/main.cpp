53d93cebd1f7a4526c8c523cb255bf02e82cc0c7
Tom Palmer <tjpalmer@tjpalmer.com>
2017-11-04 15:39:53 -0700

    Closer

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,16 @@
 #include <iostream>
+#include "tokenizer.h"
 
-int main() {
+namespace rio {
+
+void process(int argc, char** argv) {
+  Tokenizer tokenizer;
+  tokenizer.tokenize("Hi!");
+}
+
+}
+
+int main(int argc, char** argv) {
+  rio::process(argc, argv);
   std::cout << "Hello, world!" << std::endl;
 }
---
+++ src/tokenizer.h
@@ -0,0 +1,13 @@
+#include <vector>
+
+namespace rio {
+
+struct Tokenizer {
+
+  std::vector<char> tokenize(const std::string& source) {
+    return {};
+  }
+
+};
+
+}
