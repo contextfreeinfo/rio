0ff743882dedcec8178cd6b112db2ad9f4f5ff35
Tom Palmer <tjpalmer@tjpalmer.com>
2017-11-04 15:49:17 -0700

    Closer still

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,30 @@
+#include <fstream>
 #include <iostream>
+#include <sstream>
+#include <stdexcept>
 #include "tokenizer.h"
 
 namespace rio {
 
 void process(int argc, char** argv) {
+  if (argc < 2) {
+    throw std::runtime_error("source name required");
+  }
+  std::ifstream file{argv[1]};
+  std::stringstream buffer;
+  buffer << file.rdbuf();
   Tokenizer tokenizer;
-  tokenizer.tokenize("Hi!");
+  tokenizer.tokenize(buffer.str());
 }
 
 }
 
 int main(int argc, char** argv) {
-  rio::process(argc, argv);
-  std::cout << "Hello, world!" << std::endl;
+  try {
+    std::cout << "Hello, world!" << std::endl;
+    rio::process(argc, argv);
+  }
+  catch (std::exception& error) {
+    std::cout << error.what() << std::endl;
+  }
 }
