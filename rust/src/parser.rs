a7a2ec8a7b331e86b394e16bbbc4af0b1711657a
Tom <tjpalmer@tjpalmer.com>
2017-11-04 18:32:29 -0700

    Remove old parsing

--- rust/src/parser.rs
+++ rust/src/parser.rs
@@ -38,63 +38,6 @@ pub fn parse<'a>(tokens: &'a Vec<Token<'a>>) -> Node<'a> {
   let mut root = Node::new(ParseState::Expr);
   let mut parser = Parser {index: 0, tokens: tokens};
   parser.parse_block(&mut root);
-  let mut state = ParseState::Expr;
-  let mut stack: Vec<ParseState> = vec![ParseState::Expr];
-  for token in tokens {
-    let parent = &stack.last().unwrap();
-    let last = &root.kids.last();
-    let new_state = match state {
-      ParseState::Error | ParseState::Expr => {
-        match token.state {
-          TokenState::Dot => {
-            ParseState::Fraction
-          }
-          TokenState::HSpace => {
-            ParseState::Expr
-          }
-          _ => {
-            ParseState::PostAtom
-          }
-        }
-      }
-      ParseState::Fraction => {
-        match token.state {
-          TokenState::Fraction => {
-            ParseState::Expr
-          }
-          _ => {
-            ParseState::Error
-          }
-        }
-      }
-      ParseState::PostAtom => {
-        match token.state {
-          TokenState::Dot => {
-            // Get member.
-            ParseState::Expr
-          }
-          TokenState::HSpace => {
-            ParseState::PostHSpace
-          }
-          TokenState::Op1 | TokenState::Op2 => {
-            ParseState::Expr
-          }
-          _ => {
-            ParseState::Expr
-          }
-        }
-      }
-      ParseState::PostHSpace => {
-        match token.state {
-          _ => {
-            ParseState::Expr
-          }
-        }
-      }
-    };
-    println!("{:?}: {:?}", state, token);
-    state = new_state;
-  }
   root
 }
 
