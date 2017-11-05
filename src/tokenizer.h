8fc5c2bac86565e64aae78fe731714e314fad772
Tom Palmer <tjpalmer@tjpalmer.com>
2017-11-04 17:27:59 -0700

    Start tokens

--- src/tokenizer.h
+++ src/tokenizer.h
@@ -1,11 +1,46 @@
+#include <iostream>
+#include <string_view>
 #include <vector>
 
 namespace rio {
 
+typedef size_t Index;
+
+enum struct TokenState {
+  Dot,
+  Error,
+  EscapeStart,
+  Escape,
+  Fraction,
+  HSpace,
+  Id,
+  Int,
+  Op1,
+  Op2,
+  Start,
+  StringStart,
+  StringStop,
+  StringText,
+  VSpace,
+};
+
+struct Token {
+  Index col;
+  Index row;
+  Index index;
+  TokenState state;
+  std::string_view text;
+};
+
 struct Tokenizer {
 
-  std::vector<char> tokenize(const std::string& source) {
-    return {};
+  std::vector<Token> tokenize(const std::string& source) {
+    std::vector<Token> tokens;
+    for (Index i = 0; i < source.length(); ++i) {
+      auto c = source[i];
+      std::cout << c;
+    }
+    return tokens;
   }
 
 };
