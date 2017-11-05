dad6c955f09474a1a0dc6eb09252500ddf785cb7
Tom Palmer <tjpalmer@tjpalmer.com>
2017-11-04 17:46:54 -0700

    Start tokenizing port to cpp

--- src/tokenizer.h
+++ src/tokenizer.h
@@ -36,8 +36,70 @@ struct Tokenizer {
 
   std::vector<Token> tokenize(const std::string& source) {
     std::vector<Token> tokens;
+    char string_start;
+    auto state = TokenState::Start;
     for (Index i = 0; i < source.length(); ++i) {
       auto c = source[i];
+      TokenState new_state;
+      switch (state) {
+        case TokenState::Escape: {
+          new_state = TokenState::StringText;
+          break;
+        }
+        case TokenState::EscapeStart: {
+          new_state = TokenState::Escape;
+          break;
+        }
+        case TokenState::StringStart:
+        case TokenState::StringText: {
+          switch (c) {
+            case '\\': {
+              new_state = TokenState::EscapeStart;
+              break;
+            }
+            case '\'':
+            case '"': {
+              if (c == string_start) {
+                new_state = TokenState::StringStop;
+              } else {
+                new_state = TokenState::StringText;
+              }
+              break;
+            }
+            case '\r':
+            case '\n': {
+              new_state = TokenState::VSpace;
+              break;
+            }
+            default: {
+              new_state = TokenState::StringText;
+              break;
+            }
+          }
+          break;
+        }
+        default: {
+          switch (c) {
+            case ' ':
+            case '\t': {
+              new_state = TokenState::HSpace;
+              break;
+            }
+            case '\'':
+            case '"': {
+              string_start = c;
+              new_state = TokenState::StringStart;
+              break;
+            }
+            case '\r':
+            case '\n':
+            {
+              new_state = TokenState::VSpace;
+              break;
+            }
+          }
+        }
+      }
       std::cout << c;
     }
     return tokens;
