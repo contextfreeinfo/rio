3f0d4a4b666f05cc7d16c68d211c03e561ed0623
Tom <tjpalmer@tjpalmer.com>
2018-01-06 14:07:52 -0700

    Find msvs base dir

--- cpp/cc.hpp
+++ cpp/cc.hpp
@@ -4,24 +4,98 @@
 #include "sub.hpp"
 
 #ifdef _WIN32
-// # include "Setup.Configuration.h"
+# include "Setup.Configuration.h"
 #endif
 
 namespace rio {
 
 #ifdef _WIN32
 
-auto find_msvc() -> void {
-  HKEY hKey;
-  auto result = RegOpenKeyExA(HKEY_LOCAL_MACHINE, R"(SOFTWARE\WOW6432Node\Microsoft\VisualStudio\SxS\VS7)", 0, KEY_READ, &hKey);
-  if (result) {
-    throw std::runtime_error("failed to read registry");
+struct CoBstr {
+  BSTR bstr;
+  ~CoBstr() {
+    if (bstr) SysFreeString(bstr);
   }
-  std::cout << "Yo!" << std::endl;
-	// CoInitializeEx(NULL, COINIT_MULTITHREADED);
+  auto operator&() -> BSTR* {
+    return &bstr;
+  }
+};
 
-  // ISetupInstance setup_config;
-  // CoCreateInstance(__uuidof(SetupConfiguration), nullptr, CLSCTX_INPROC_SERVER, IID_ISetupInstance, &setup_config);
+template<typename Interface>
+struct CoInstance {
+  Interface* instance = nullptr;
+  ~CoInstance() {
+    if (instance) {
+      // std::cout << "releasing an instance" << std::endl;
+      instance->Release();
+    }
+  }
+  auto operator->() -> Interface* {
+    return instance;
+  }
+  auto operator&() -> Interface** {
+    return &instance;
+  }
+  operator bool() {
+    return instance;
+  }
+  operator void**() {
+    return reinterpret_cast<void**>(&instance);
+  }
+};
+
+auto find_msvc() -> void {
+  // Based on Setup.Configuration.ZeroDeps.VC.cpp.
+  // Not using atl since it seems not always available by default (such as for
+  // my setup), and I want easy building.
+  if (CoInitializeEx(NULL, COINIT_MULTITHREADED)) {
+    throw std::runtime_error("no com");
+  }
+  CoInstance<ISetupConfiguration2> setup_config;
+  if (CoCreateInstance(
+    __uuidof(SetupConfiguration), nullptr, CLSCTX_INPROC_SERVER,
+    IID_ISetupConfiguration2, setup_config
+  )) {
+    // TODO Check for older visual studio versions in the registry.
+    throw std::runtime_error("no setup config");
+  }
+  CoInstance<ISetupHelper> setup_helper;
+  if (setup_config->QueryInterface(IID_ISetupHelper, setup_helper)) {
+    throw std::runtime_error("no setup helper");
+  }
+  CoInstance<IEnumSetupInstances> enum_instances;
+  if (setup_config->EnumAllInstances(&enum_instances)) {
+    throw std::runtime_error("enum instances failed");
+  }
+  while (true) {
+    CoInstance<ISetupInstance> instance;
+    if (!enum_instances->Next(1, &instance, nullptr) && instance) {
+      // std::cout << "Got an instance!" << std::endl;
+      CoInstance<ISetupInstance2> instance2;
+      if (instance->QueryInterface(IID_ISetupInstance2, instance2)) {
+        throw std::runtime_error("instance2 failed");
+      }
+      std::cout << "Got instance2" << std::endl;
+      CoBstr wpath;
+      if (instance2->GetInstallationPath(&wpath)) {
+        throw std::runtime_error("instance path failed");
+      }
+      auto path = wstr_to_u8(wpath.bstr);
+      std::cout << "path: " << path << std::endl;
+    } else {
+      break;
+    }
+  }
+  // HKEY hKey;
+  // auto result = RegOpenKeyExW(
+  //   HKEY_LOCAL_MACHINE,
+  //   LR"(SOFTWARE\WOW6432Node\Microsoft\VisualStudio\SxS\VS7)",
+  //   0, KEY_READ, &hKey
+  // );
+  // if (result) {
+  //   throw std::runtime_error("failed to read registry");
+  // }
+  std::cout << "Yo!!" << std::endl;
 }
 
 auto compile_c(const fs::path& path, bool verbose = false) -> void {
--- cpp/dirs.hpp
+++ cpp/dirs.hpp
@@ -21,39 +21,6 @@ namespace rio {
 
 #ifdef _WIN32
 
-auto get_cache_dir() -> fs::path {
-  // Get the path as wstr.
-  PWSTR wide;
-  auto result = SHGetKnownFolderPath(FOLDERID_LocalAppData, 0, NULL, &wide);
-  if (result != S_OK) {
-    CoTaskMemFree(wide);
-    throw std::runtime_error("no cache dir");
-  }
-  // Find utf8 size.
-  // TODO Use std c++ functions?
-  auto size = WideCharToMultiByte(CP_UTF8, 0, wide, -1, 0, 0, NULL, NULL);
-  if (!size) {
-    CoTaskMemFree(wide);
-    throw std::runtime_error("bad utf8 size");
-  }
-  // Convert to utf8, freeing wide after.
-  std::string bytes;
-  // Make the size large enough, not just the reserved space.
-  // C++11 and later require implicit null char after string content, so this
-  // is safe.
-  bytes.assign(static_cast<size_t>(size), ' ');
-  auto new_size =
-    WideCharToMultiByte(CP_UTF8, 0, wide, -1, bytes.data(), size, NULL, NULL);
-  // Free now that we're done.
-  CoTaskMemFree(wide);
-  // And still fail if things died.
-  if (new_size != size) {
-    throw std::runtime_error("bad utf8 size");
-  }
-  // Good to go.
-  return bytes;
-}
-
 auto u8_to_wstr(std::string_view u8) -> std::wstring {
   // Convert to utf16.
   std::wstring wstr;
@@ -74,6 +41,46 @@ auto u8_to_wstr(std::string_view u8) -> std::wstring {
     throw std::runtime_error("bad utf16 size");
   }
   return wstr;
+}
+
+auto wstr_to_u8(PWSTR wide, bool free = false) -> std::string {
+  // Convert to utf8.
+  std::string u8;
+  if (!wide) {
+    // Treat null as empty.
+    return u8;
+  }
+  // Find utf8 size.
+  // TODO Use std c++ functions?
+  auto size = WideCharToMultiByte(CP_UTF8, 0, wide, -1, 0, 0, NULL, NULL);
+  if (!size) {
+    if (free) CoTaskMemFree(wide);
+    throw std::runtime_error("bad utf8 size");
+  }
+  // Make the size large enough, not just the reserved space.
+  // C++11 and later require implicit null char after string content, so this
+  // is safe.
+  u8.assign(static_cast<size_t>(size), ' ');
+  auto new_size =
+    WideCharToMultiByte(CP_UTF8, 0, wide, -1, u8.data(), size, NULL, NULL);
+  // Free if requested before leaving.
+  if (free) CoTaskMemFree(wide);
+  // And still fail if things died.
+  if (new_size != size) {
+    throw std::runtime_error("bad utf8 size");
+  }
+  return u8;
+}
+
+auto get_cache_dir() -> fs::path {
+  // Get the path as wstr.
+  PWSTR wide;
+  auto result = SHGetKnownFolderPath(FOLDERID_LocalAppData, 0, NULL, &wide);
+  if (result != S_OK) {
+    CoTaskMemFree(wide);
+    throw std::runtime_error("no cache dir");
+  }
+  return wstr_to_u8(wide, true);
 }
 
 // #elif __APPLE__ // TODO What for Apple?
