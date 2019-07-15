8fb4641a615b127c0686c5b8c253ab4d394cd79b
Tom <tjpalmer@tjpalmer.com>
2019-07-15 07:06:24 -0700

    Regen test output

--- tests/test1.c
+++ tests/test1.c
@@ -2,9 +2,10 @@
 #include <stdio.h>
 
 const char* const message = "Hello";
+
 void main() {
   const char* const name = "world";
-  const char* const age = 75;
+  int32_t const age = 75;
   printf("%s, %s!\n", message, name);
   printf("You are %d years old.\n", age);
 }
