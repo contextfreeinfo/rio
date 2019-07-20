e7c8b8a0706baca1573a91be6143386a133d18ac
Tom <tjpalmer@tjpalmer.com>
2019-07-20 13:26:03 -0700

    Update test results

--- tests/test1.c
+++ tests/test1.c
@@ -5,13 +5,13 @@ int main();
 
 void greet();
 
-const char* const message = "Hello";
-
 int main() {
   const char* const name = "world";
   int32_t const age = 75;
   greet(name, age);
 }
+
+const char* const message = "Hello";
 
 void greet(const char* const name, int32_t const age) {
   printf("%s, %s!\n", message, name);
