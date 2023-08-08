2081dad55235a0fbbf04f4db1a264e4d2f2e16fd
Tom <tom@contextfree.info>
2023-08-07 21:19:12 -0700

    Expand def types

--- src/norm.rs
+++ src/norm.rs
@@ -1,7 +1,7 @@
 use std::ops::Range;
 
 use crate::{
-    lex::TokenKind,
+    lex::{Token, TokenKind},
     tree::{BranchKind, Node, TreeBuilder},
     Cart,
 };
@@ -16,17 +16,84 @@ impl Normer {
     }
 
     pub fn norm(&mut self, tree: &mut Vec<Node>) {
-        self.builder().clear();
-        self.any(&tree, 0);
-        self.builder().wrap(BranchKind::Block, 0);
-        self.builder().drain_into(tree);
+        self.trim(tree);
+        self.define(tree);
     }
 
     fn builder(&mut self) -> &mut TreeBuilder {
         &mut self.cart.tree_builder
     }
 
-    fn any(&mut self, tree: &[Node], index: usize) -> Option<()> {
+    fn define(&mut self, tree: &mut Vec<Node>) {
+        self.builder().clear();
+        self.define_at(&tree);
+        self.builder().wrap(BranchKind::Block, 0);
+        self.builder().drain_into(tree);
+    }
+
+    fn define_at(&mut self, tree: &[Node]) -> Option<()> {
+        match *tree.last()? {
+            Node::Branch { kind, range } => match kind {
+                BranchKind::Def => {
+                    let start = self.builder().pos();
+                    let range: Range<usize> = range.into();
+                    // We can't parse a Def of range len 0.
+                    let first = tree[range.start];
+                    let range = match first {
+                        Node::Branch {
+                            kind: BranchKind::Typed,
+                            range: first_range,
+                        } => {
+                            // Expand typed in place.
+                            let first_range: Range<usize> = first_range.into();
+                            for kid_index in first_range.clone() {
+                                self.define_at(&tree[..kid_index + 1]);
+                            }
+                            range.start + 1..range.end
+                        }
+                        Node::Leaf {
+                            token:
+                                token @ Token {
+                                    kind: TokenKind::Id,
+                                    ..
+                                },
+                        } => {
+                            self.builder().push(token);
+                            // Untyped, so push empty type after id.
+                            self.builder().push_none();
+                            range.start + 1..range.end
+                        }
+                        _ => range,
+                    };
+                    for kid_index in range.clone() {
+                        self.define_at(&tree[..kid_index + 1]);
+                    }
+                    self.builder().wrap(kind, start);
+                }
+                _ => {
+                    // TODO Change Typed to Def with separate type?
+                    // TODO Change all Def to triples? Quads? Kids with meta lists?
+                    let start = self.builder().pos();
+                    let range: Range<usize> = range.into();
+                    for kid_index in range.clone() {
+                        self.define_at(&tree[..kid_index + 1]);
+                    }
+                    self.builder().wrap(kind, start);
+                }
+            },
+            node @ _ => self.builder().push(node),
+        }
+        Some(())
+    }
+
+    fn trim(&mut self, tree: &mut Vec<Node>) {
+        self.builder().clear();
+        self.trim_at(&tree, 0);
+        self.builder().wrap(BranchKind::Block, 0);
+        self.builder().drain_into(tree);
+    }
+
+    fn trim_at(&mut self, tree: &[Node], index: usize) -> Option<()> {
         let root = *tree.last()?;
         match root {
             Node::Branch { kind, range } => {
@@ -35,7 +102,7 @@ impl Normer {
                 let start = self.builder().pos();
                 let range: Range<usize> = range.into();
                 for kid_index in range.clone() {
-                    self.any(&tree[..kid_index + 1], kid_index - range.start);
+                    self.trim_at(&tree[..kid_index + 1], kid_index - range.start);
                 }
                 self.builder().wrap(kind, start);
             }
