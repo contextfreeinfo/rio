5d0c75548633f008115d609b0581c68116d2ae02
Tom <tom@contextfree.info>
2023-11-11 18:45:41 -0700

    Organize building; generate import

--- examples/out/hi.wat
+++ examples/out/hi.wat
@@ -1,3 +1,4 @@
 (module
   (type (;0;) (func (param i32 i32 i32 i32) (result i32)))
+  (import "wasi_snapshot_preview1" "fd_write" (func (;0;) (type 0)))
 )
--- src/wasm.rs
+++ src/wasm.rs
@@ -6,37 +6,24 @@ use std::{
 
 use anyhow::{Error, Ok, Result};
 use wasm_encoder::{
-    CodeSection, ExportSection, FunctionSection, ImportSection, TypeSection, ValType,
+    CodeSection, EntityType, ExportSection, FunctionSection, ImportSection, TypeSection, ValType,
 };
 
 use crate::{BuildArgs, Cart};
 
 pub fn write_wasm(args: &BuildArgs, cart: &Cart) -> Result<()> {
+    let _ = cart;
+    let mut writer = WasmWriter::new();
+    writer.build()?;
+    let wasm = writer.module.finish();
+    write_out(args, wasm)
+}
+
+fn write_out(args: &BuildArgs, wasm: Vec<u8>) -> Result<()> {
     // TODO Default to current dir?
     let Some(outdir) = &args.outdir else {
         return Ok(());
     };
-    let mut module = wasm_encoder::Module::new();
-    let _ = cart;
-    // Types
-    let mut types = TypeSection::new();
-    add_fd_write_type(&mut types);
-    module.section(&types);
-    // Imports
-    let mut imports = ImportSection::new();
-    add_fd_write_import(&mut imports);
-    module.section(&imports);
-    // Functions
-    let functions = FunctionSection::new();
-    module.section(&functions);
-    // Exports
-    let exports = ExportSection::new();
-    module.section(&exports);
-    // Codes
-    let codes = CodeSection::new();
-    module.section(&codes);
-    // Finish
-    let wasm = module.finish();
     create_dir_all(outdir)?;
     let name = Path::new(args.app.as_str())
         .file_stem()
@@ -49,8 +36,63 @@ pub fn write_wasm(args: &BuildArgs, cart: &Cart) -> Result<()> {
     Ok(())
 }
 
-fn add_fd_write_import(imports: &mut ImportSection) {
-    // imports.import("wasi_snapshot_preview1", "fd_write", ty)
+struct WasmWriter {
+    fd_write_type: Option<EntityType>,
+    module: wasm_encoder::Module,
+}
+
+impl WasmWriter {
+    fn new() -> WasmWriter {
+        WasmWriter {
+            fd_write_type: None,
+            module: wasm_encoder::Module::new(),
+        }
+    }
+
+    fn build(&mut self) -> Result<()> {
+        self.build_types();
+        self.build_imports();
+        self.build_functions();
+        self.build_exports();
+        self.build_codes();
+        Ok(())
+    }
+
+    fn add_fd_write_import(&self, imports: &mut ImportSection) {
+        imports.import(
+            "wasi_snapshot_preview1",
+            "fd_write",
+            self.fd_write_type.unwrap(),
+        );
+    }
+
+    fn build_codes(&mut self) {
+        let codes = CodeSection::new();
+        self.module.section(&codes);
+    }
+
+    fn build_exports(&mut self) {
+        let exports = ExportSection::new();
+        self.module.section(&exports);
+    }
+
+    fn build_functions(&mut self) {
+        let functions = FunctionSection::new();
+        self.module.section(&functions);
+    }
+
+    fn build_imports(&mut self) {
+        let mut imports = ImportSection::new();
+        self.add_fd_write_import(&mut imports);
+        self.module.section(&imports);
+    }
+
+    fn build_types(&mut self) {
+        let mut types = TypeSection::new();
+        add_fd_write_type(&mut types);
+        self.fd_write_type = Some(EntityType::Function(0));
+        self.module.section(&types);
+    }
 }
 
 fn add_fd_write_type(types: &mut TypeSection) {
