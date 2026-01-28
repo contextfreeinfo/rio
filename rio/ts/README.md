ffed1ed58b6326dcddd016564fd156b5b9328e7e
Tom <tom@contextfree.info>
2026-01-28 08:16:02 -0700

    Start project in bun

---
+++ rio/ts/README.md
@@ -0,0 +1,15 @@
+# rio
+
+Still incomplete setup for running rio from JS, including in browsers.
+
+To build:
+
+```bash
+bun build.ts
+```
+
+To run:
+
+```bash
+bun dist/rio.js
+```
---
+++ rio/ts/index.ts
@@ -0,0 +1,21 @@
+import * as engine from "rio-engine";
+
+function main() {
+    const interner = new engine.Interner();
+
+    // Lex
+    const lexer = new engine.Lexer(interner);
+    const tokens = lexer.lex(engine.hi);
+
+    // Parse
+    const parser = new engine.Parser();
+    const parseTree = parser.parse(tokens);
+    // console.log(engine.ParseNode.stringifyTree(parseTree, interner));
+
+    // Norm
+    const normer = new engine.Normer(interner);
+    const normed = normer.norm(parseTree);
+    console.log(engine.Node.stringifyTree(normed.nodes, interner));
+}
+
+main()
