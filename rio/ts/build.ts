4088e37c273e95997cd05121c5755df267e43100
Tom <tom@contextfree.info>
2026-01-28 08:19:02 -0700

    Adjust build slightly

--- rio/ts/build.ts
+++ rio/ts/build.ts
@@ -6,4 +6,4 @@ await $`bun install`.cwd("../../engine/temper.out/js/rio-engine");
 // Then just install and build here.
 await $`bun install`;
 // Shell out so we don't live on a cached version of node_modules.
-await $`bun build --minify ./index.ts --outfile ./dist/rio.js`;
+await $`bun build --production index.ts --outfile dist/rio.js`;
