3cbf30161078c38ec2b3eeb7966073c29910afb8
Tom <tom@contextfree.info>
2026-07-03 06:50:46 -0700

    Try and mostly fail at tusb

---
+++ pico/tusb_config.h
@@ -0,0 +1,11 @@
+#ifndef _TUSB_CONFIG_H_
+#define _TUSB_CONFIG_H_
+
+#define CFG_TUSB_MCU       OPT_MCU_RP2350
+#define CFG_TUH_ENABLED    1
+#define CFG_TUD_ENABLED    0
+#define CFG_TUH_MSC        1
+#define CFG_TUH_DEVICE_MAX 2
+#define CFG_TUH_HUB        1
+
+#endif // _TUSB_CONFIG_H_
