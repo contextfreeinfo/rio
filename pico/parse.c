a5f326e66aa37ef782aee3d223527273cba7e00b
Tom <tom@contextfree.info>
2026-07-08 19:44:18 -0700

    Work toward parsing; think on gen

--- pico/parse.c
+++ pico/parse.c
@@ -3,14 +3,18 @@
 #include "lex.h"
 
 uint8_t rio_archcode[rio_archcode_size] = {0};
-uint8_t rio_bytecode[rio_bytecode_size] = {0};
+uint8_t rio_globals[rio_globals_size] = {0};
 uint16_t rio_names[rio_names_size] = {0};
 uint8_t rio_text[rio_text_size] = {0};
 
-rio_Err rio_parse(rio_File file) {
-    rio_Lexer lexer = { .file = file };
-    while (!rio_lexNext(&lexer)) {
-        rio_Token token = lexer.token;
+typedef struct rio_Parser {
+    rio_Lexer lexer;
+} rio_Parser;
+
+rio_Err rio_parserAdvance(rio_Parser* parser) {
+    rio_Err err = 0;
+    while (!(err = rio_lexNext(&parser->lexer))) {
+        rio_Token token = parser->lexer.token;
         printf(
             "%s (%d); %zu..%zu\n",
             token.text,
@@ -18,6 +22,47 @@ rio_Err rio_parse(rio_File file) {
             token.start,
             token.end
         );
+        if (token.kind != rio_TokenKind_space) goto done;
+    }
+    done:
+    return err;
+}
+
+rio_Err rio_parseNameStatement(rio_Parser* parser) {
+    rio_Err err = 0;
+    printf("Got name: %s\n", parser->lexer.token.text);
+    while (!(err = rio_parserAdvance(parser))) {
+        rio_Token token = parser->lexer.token;
+        if (token.kind == rio_TokenKind_endLine) goto statementDone;
+    }
+    statementDone:
+    printf("Name statement done\n");
+    return err;
+}
+
+rio_Err rio_parseStatement(rio_Parser* parser) {
+    rio_Err err = 0;
+    err = rio_parserAdvance(parser);
+    if (err) return err;
+    switch (parser->lexer.token.kind) {
+    case rio_TokenKind_name:
+        return rio_parseNameStatement(parser);
+    default:
+        break;
+    }
+    while (!(err = rio_parserAdvance(parser))) {
+        rio_Token token = parser->lexer.token;
+        if (token.kind == rio_TokenKind_endLine) goto statementDone;
+    }
+    statementDone:
+    printf("Statement done\n");
+    return err;
+}
+
+rio_Err rio_parse(rio_File file) {
+    rio_Parser parser = { .lexer = { .file = file } };
+    while (!rio_parseStatement(&parser)) {
+        // Keep going.
     }
     return 0;
 }
--- pico/parse.h
+++ pico/parse.h
@@ -4,12 +4,15 @@
 #include "sys.h"
 
 #define rio_archcode_size 0x100000
-#define rio_bytecode_size rio_archcode_size
+#define rio_globals_size 0x1000
 #define rio_names_size 0x10000
 #define rio_text_size 0x10000
 
 extern uint8_t rio_archcode[rio_archcode_size];
-extern uint8_t rio_bytecode[rio_bytecode_size];
+
+extern uint8_t rio_globals[rio_globals_size]; // TODO Mix with names?
+// If each module top-level or even field def ends up here, are we good?
+
 extern uint16_t rio_names[rio_names_size];
 extern uint8_t rio_text[rio_text_size];
 
