667021f6a657947107934b86f616cff5cdf28fe5
Tom <tom@contextfree.info>
2026-09-11 20:12:50 -0700

    Use explicit dir

--- library.cmake
+++ library.cmake
@@ -1,9 +1,9 @@
 add_library(rio-lib INTERFACE)
 
 target_sources(rio-lib INTERFACE
-    src/engine.c
-    src/gen.c
-    src/lex.c
-    src/parse.c
-    src/util.c
+    ${CMAKE_CURRENT_LIST_DIR}/src/engine.c
+    ${CMAKE_CURRENT_LIST_DIR}/src/gen.c
+    ${CMAKE_CURRENT_LIST_DIR}/src/lex.c
+    ${CMAKE_CURRENT_LIST_DIR}/src/parse.c
+    ${CMAKE_CURRENT_LIST_DIR}/src/util.c
 )
